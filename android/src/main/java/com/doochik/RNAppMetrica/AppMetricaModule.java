6cf89b7379f796d5287c07180a75c457a88b1499
ZloyRob <vadimandsanek@yandex.ru>
2019-11-06 14:22:25 +0700

    fix Nullable import

--- android/src/main/java/com/doochik/RNAppMetrica/AppMetricaModule.java
+++ android/src/main/java/com/doochik/RNAppMetrica/AppMetricaModule.java
@@ -2,7 +2,7 @@ package com.doochik.RNAppMetrica;
 
 import android.app.Activity;
 import android.app.Application;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 
 import com.facebook.react.bridge.ReactApplicationContext;
